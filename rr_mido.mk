c2c0aff1ea55430d15f2d446003f382d39aae5d9
beta <mramees34@gmail.com>
2020-08-12 08:17:19 +0000

    mido:Enable Face Unlock

--- rr_mido.mk
+++ rr_mido.mk
@@ -24,6 +24,9 @@ $(call inherit-product, device/xiaomi/mido/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORT := true
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
 PRODUCT_NAME := rr_mido
